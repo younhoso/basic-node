a0471ad1de792457c8eaa5ea02b4394b69f7baa2
younhoso <thdbsgh3443@naver.com>
2022-03-04 14:41:30 +0900

    detail 페이지에서 잘 못된 url로 접속시 404페이지로 이동

--- routes/detail.js
+++ routes/detail.js
@@ -7,11 +7,11 @@ router.get('/:id', async (req, res) => {
 		const data = await todoModel.findOne({id: parseInt(req.params.id)}).exec();
 		res.render('products/detail',{'detail_data': data})
 		if(!data){
-			res.status(404).redirect('/notPage')
+			res.status(404).render('error', {'massage': '잘 못된 페이지 입니다.'});
 		}
 	} catch (e) {
 		res.status(500).json({
-      message: "User 조회 실패",
+      message: "data 조회 실패",
     });
 	}
 });
