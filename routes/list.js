aca4c4f2eac33b28c998be054b8c3243edc008e2
younhoso <thdbsgh3443@naver.com>
2022-03-07 23:25:31 +0900

    글 수정 기능 2 : DB 데이터를 수정

--- routes/list.js
+++ routes/list.js
@@ -8,10 +8,15 @@ router.get('/' , (_, res) => {
 	.catch(err => res.status(500).send(err))
 });
 
+router.delete('/delete/:id', (req, res) => {
+	todoModel.deleteOne({id : parseInt(req.params.id)})
+	.then(data => res.json({ message : "success" }))
+	.catch(err => res.status(500).send(err))
+});
+
 router.get('/edit/:id', async (req, res) => {
 	try {
 		const data = await todoModel.findOne({id : parseInt(req.params.id)}).exec();
-		console.log(data)
 		res.render('admin/edit', {post: data})
 		if(!data){
 			res.status(404).render('error', {'massage': '잘 못된 페이지 입니다.'});
@@ -23,10 +28,14 @@ router.get('/edit/:id', async (req, res) => {
 	}
 });
 
-router.delete('/delete/:id', (req, res) => {
-	todoModel.deleteOne({id : parseInt(req.params.id)})
-	.then(data => res.json({ message : "success" }))
-	.catch(err => res.status(500).send(err))
+router.put('/edit', async (req, res) => {
+	// 폼에담긴 제목데이터, 날짜데이터를 가지고
+	try {
+		await todoModel.updateOne({id: parseInt(req.body.id)},{$set: {title: req.body.title, desc: req.body.desc}}).exec();
+		res.redirect('/list')
+	} catch (e) {
+	
+	}
 });
 
 module.exports = router;
